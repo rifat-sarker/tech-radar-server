5942442ab91162f096f8a842cab357f00ba4fbed
unknown <rifat-sarker>
2023-12-15 21:07:49 +0600

    get request of update product and send updated product to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/product/:id', async(req, res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.findOne(query)
+        res.send(result)
+    })
+
     app.post('/product',async(req,res)=>{
         const newProduct = req.body;
         console.log(newProduct);
@@ -42,6 +49,26 @@ async function run() {
         res.send(result);
     })
 
+    app.put('/product/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+
+      const product = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type : updatedProduct.type,
+          price : updatedProduct.price,
+          description : updatedProduct.description,
+          rating : updatedProduct.rating,
+          imageURL: updatedProduct.imageURL
+        }
+      }
+      const result = await productCollection.updateOne(filter, product, options)
+      res.send(result)
+    } )
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
